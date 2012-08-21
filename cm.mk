ad65f3a5b928e5b7312258baeab78cab20b2e6e4
freexperia <jerpelea@gmail.com>
2012-08-21 18:35:20 +0300

    tapioca_ss: new bootanimations

--- cm.mk
+++ cm.mk
@@ -8,7 +8,7 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=ST21i_1257-4009 BUILD_FINGERPRINT=SEMC/ST21i_1257-4009/ST21i:4.0.4/6.1.A.0.452/O_5_zw:user/release-keys PRIVATE_BUILD_DESC="ST21i-user 4.0.4 6.1.A.0.452 O_5_zw test-keys"
 
-TARGET_BOOTANIMATION_NAME := vertical-320x480
+TARGET_BOOTANIMATION_NAME := vertical-320
 
 PRODUCT_NAME := cm_tapioca_ss
 PRODUCT_DEVICE := tapioca_ss
