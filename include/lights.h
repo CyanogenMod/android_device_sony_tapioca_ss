7d8359731062d8485025a80629a3139099db5c22
Alin Jerpelea <jerpelea@gmail.com>
2013-01-19 10:13:09 +0200

    tapioca: lights typo fix
    Change-Id: Idf95e27400114c8ff0595e5d8d98aaad68a15aa2

--- include/lights.h
+++ include/lights.h
@@ -1,16 +1,16 @@
-char const*const RED_LED_ON          = "01 4 1";
-char const*const GREEN_LED_ON        = "01 5 1";
-char const*const BLUE_LED_ON         = "01 6 1";
-char const*const RED_LED_OFF         = "01 4 0";
-char const*const GREEN_LED_OFF       = "01 5 0";
-char const*const BLUE_LED_OFF        = "01 6 0";
+char const*const RED_LED_ON          = "01 0 1";
+char const*const GREEN_LED_ON        = "01 1 1";
+char const*const BLUE_LED_ON         = "01 2 1";
+char const*const RED_LED_OFF         = "01 0 0";
+char const*const GREEN_LED_OFF       = "01 1 0";
+char const*const BLUE_LED_OFF        = "01 2 0";
 
-char const*const RED_LED_BLINK_ON    = "02 4 1";
-char const*const GREEN_LED_BLINK_ON  = "02 5 1";
-char const*const BLUE_LED_BLINK_ON   = "02 6 1";
-char const*const RED_LED_BLINK_OFF   = "02 4 0";
-char const*const GREEN_LED_BLINK_OFF = "02 5 0";
-char const*const BLUE_LED_BLINK_OFF  = "02 6 0";
+char const*const RED_LED_BLINK_ON    = "02 0 1";
+char const*const GREEN_LED_BLINK_ON  = "02 1 1";
+char const*const BLUE_LED_BLINK_ON   = "02 2 1";
+char const*const RED_LED_BLINK_OFF   = "02 0 0";
+char const*const GREEN_LED_BLINK_OFF = "02 1 0";
+char const*const BLUE_LED_BLINK_OFF  = "02 2 0";
 
 char const*const KEY_LED_ON          = "01 3 1";
 char const*const KEY_LED_OFF         = "01 3 0";
